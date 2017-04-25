f2b1677b8e62f9855448f86190021b29d5949eb0
Dominic Brown and Sean Pierce <dominic+sean@>
2017-04-25 11:21:23 -0700

    add code for first spec

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,9 +1,46 @@
-# example ruby code
 
-# class Palindrome
+class Tama
 
-#   def is_word?(user_input)
-#     user_input.match?(/[aeiouy]+/i)
-#   end
+  @@all_Tama
 
-# end
+  define_method(:initialize) do |name|
+    @name = name
+    @lvl = 0
+    @alive = true
+    @health = 10
+    @fed_lvl = 10
+  end
+
+  define_method(:play) do
+    "it's fun to play"
+  end
+
+  define_method(:feed) do
+    "mmm arigato gozaimasu"
+  end
+
+  define_method(:sleep) do
+    "zzzZZZzzz"
+  end
+
+  define_method(:name) do
+    @name
+  end
+
+  define_method(:lvl) do
+    @lvl
+  end
+
+  define_method(:health) do
+    @health
+  end
+
+  define_method(:alive?) do
+    @alive
+  end
+
+  define_method(:fed_lvl) do
+    @fed_lvl
+  end
+
+end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -3,17 +3,38 @@ require "rspec"
 require "pry"
 
 
-# example project spec
+describe(Tama) do
 
-# describe 'Palindrome#isWord?' do
-#   let(:word) { Palindrome.new }
+  describe("#initialize") do
+    it("sets the name and life levels of a new Tamagotchi") do
+      my_pet = Tama.new("lipper")
+      expect(my_pet.name()).to(eq("lipper"))
+      expect(my_pet.lvl()).to(eq(0))
+      expect(my_pet.alive?()).to(eq(true))
+      expect(my_pet.health()).to(eq(10))
+      expect(my_pet.fed_lvl()).to(eq(10))
+    end
+  end
 
-#   it "returns true if the word has at least one vowel" do
-#     expect(word.is_word?("word")).to eq true
-#   end
+  # describe("#is_alive") do
+  #   it("is alive if the food level is above 0") do
+  #     my_pet = Tamagotchi.new("lil dragon")
+  #     expect(my_pet.is_alive()).to(eq(true))
+  #   end
+  #
+  #   it("is dead if the food level is 0") do
+  #     my_pet = Tamagotchi.new("lil dragon")
+  #     my_pet.set_food_level() #make a method that will change the food level of your tamagotchi.
+  #     expect(my_pet.is_alive()).to(eq(false))
+  #   end
+  # end
+  #
+  # describe("#time_passes") do
+  #   it("decreases the amount of food the Tamagotchi has left by 1") do
+  #     my_pet = Tamagotchi.new("lil dragon")
+  #     my_pet.time_passes()  #decide what trigger you will use to make time pass
+  #     expect(my_pet.food_level()).to(eq(9))
+  #   end
+  # end
 
-#   it "returns false if the word does not have a vowel" do
-#     expect(word.is_word?("wrd")).to eq false
-#   end
-
-# end
+end
