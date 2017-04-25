72296ef47b7389389e326a10890fa138a8d2b978
Dominic Brown and Sean Pierce <dominic@>
2017-04-25 13:47:21 -0700

    add code for persistant data and current time

--- app.rb
+++ app.rb
@@ -12,29 +12,34 @@ end
 get('/output') do
   @tama = Tama.new(params.fetch('tama_name'))
   Tama.add_tama(@tama)
-  @tama_lvl = @tama.lvl
-  @tama_name = @tama.name
-  @tama_health = @tama.health
-  @tama_fedlvl = @tama.fed_lvl
   erb(:output)
 end
 
 get('/feed') do
-  @tama2 = Tama.return_tama_at_index(0)
-  @com = @tama2.feed
-  @tama_lvl = @tama2.lvl
-  @tama_name = @tama2.name
-  @tama_health = @tama2.health
-  @tama_fedlvl = @tama2.fed_lvl
+  @tama = Tama.return_tama_at_index(0)
+  @com = @tama.feed
+  # redirect('/output')
   erb(:output)
 end
 
 get('/play') do
+  @tama = Tama.return_tama_at_index(0)
   @com = @tama.play
+  # redirect('/output')
   erb(:output)
 end
 
 get('/sleep') do
+  @tama = Tama.return_tama_at_index(0)
   @com = @tama.zzz
+  # redirect('/output')
+  erb(:output)
+end
+
+get('/time') do
+  @tama = Tama.return_tama_at_index(0)
+  @com = ""
+  @time = Time.new()
+  # redirect('/output')
   erb(:output)
 end
